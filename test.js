76511a4a901a734c8fd004e4aafd55c1a146fcb8
Nandkumar Ghatge <Nandkumar.ghatge@smartbox.com>
2019-11-08 14:05:29 +0000

    added new project

---
+++ test.js
@@ -0,0 +1,33 @@
+var newman = require('newman');
+
+function runcollection(callback){
+   newman.run({
+      collection: 'C:\\newman\\PMP Dependency latest collection\\Updated\\TestCollection.postman_collection.json',
+      environment: 'C:\\newman\\PMP Dependency latest collection\\Updated\\Test.postman_environment.json',
+      iterationCount :1
+   },function(error, summary){
+      callback(summary)
+   })
+   // .on('beforeRequest', function(err, req){
+   //    if(req.request.url.path[4] === 'voucher_creation'){
+   //       const jbody = req.request.body.raw;
+   //       const arrybody= JSON.parse(jbody);
+   //       arrybody.actor.id = 45878
+   //       req.request.body.raw = JSON.stringify(arrybody);
+   //    }
+   // })
+}
+
+describe('newman', function () {
+   // done on the next line is a function that marks the current test as asynchronous
+   it('Generate voucher with 200 response', function () {
+      runcollection(result => {console.log(result.run.executions[0].response.stream.toString())})
+       //runcollection(result)
+
+   });
+
+   it('generate voucher with 502', function () {
+      //runcollection(result => console.log(result.run.executions[0].response.stream.toString()))
+     
+   });
+});
