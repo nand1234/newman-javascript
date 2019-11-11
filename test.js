4cfee7c6f872bc4395f03cf5c7991b9a02d7b14a
nand1234 <31034303+nand1234@users.noreply.github.com>
2019-11-11 16:56:37 +0000

    Update test.js

--- test.js
+++ test.js
@@ -1,16 +1,16 @@
 var newman = require('newman');
 
+
 function runcollection(callback){
    newman.run({
-      collection: 'C:\\newman\\PMP Dependency latest collection\\Updated\\TestCollection.postman_collection.json',
-      environment: 'C:\\newman\\PMP Dependency latest collection\\Updated\\Test.postman_environment.json',
-      iterationCount :1
+   collection: 'C:\\newman\\PMP Dependency latest collection\\Updated\\TestCollection.postman_collection.json',
+   environment: 'C:\\newman\\PMP Dependency latest collection\\Updated\\Test.postman_environment.json',
+   iterationCount :1
    },function(error, summary){
-      callback(summary)
+      callback(summary);
    })
-   // if you want to modify any request you can do using below code
    // .on('beforeRequest', function(err, req){
-   //    if(req.request.url.path[4] === 'name of the request to be modify'){
+   //    if(req.request.url.path[4] === 'voucher_creation'){
    //       const jbody = req.request.body.raw;
    //       const arrybody= JSON.parse(jbody);
    //       arrybody.actor.id = 45878
@@ -19,16 +19,29 @@ function runcollection(callback){
    // })
 }
 
+before(function(){
+
+});
+
 describe('newman', function () {
+    var res = new Array();
    // done on the next line is a function that marks the current test as asynchronous
-   it('Generate voucher with 200 response', function () {
-      runcollection(result => {console.log(result.run.executions[0].response.stream.toString())})
-       //runcollection(result)
+   it('Generate voucher with 200 response', function (done) {
+      runcollection(function(result){
+         x = result.run.executions[0].response.stream.toString();
+        //console.log(result)
+        handleData(x)
+        done();
+     });
+
+     function handleData(data) {
+        console.log(data);
+      }
 
    });
 
-   it('generate voucher with 502', function () {
-      //runcollection(result => console.log(result.run.executions[0].response.stream.toString()))
-     
+   it('Generate voucher with 200 response', function (done) {
+      console.log('2nd test passed');
+      done();
    });
 });
